a2be7b5960e353805d71683cc37e8f1cf2ce708c
carolinamalin <carolinamalinconico@gmail.com>
2023-05-30 10:47:08 -0300

    input

--- evento.py
+++ evento.py
@@ -8,11 +8,10 @@ class Evento:
         icms = custo * 0.18  # 18% do total
         cofins = custo * 0.03  # 3% do total
         pis = custo * 0.0065  # 0.65% do total
-
         return iss + icms + cofins + pis
 
     def ingressos(self, quantidade, receita):
-        ingresso = (receita * 0,6)/ quantidade
+        ingresso = (receita * 0.6)/ quantidade
         return ingresso + self.imposto_total(ingresso)
 
     def custo(self, quantidade_pessoas, atracao):
@@ -20,45 +19,50 @@ class Evento:
         custo_bombeiro = quantidade_bombeiros * 140
         quantidade_policia = quantidade_pessoas/200
         custo_policia = quantidade_policia * 200
-        custo_aluguel = quantidade_pessoas/3,5 * 40
+        custo_aluguel = quantidade_pessoas/3.5 * 40
         custo_festa = custo_bombeiro + custo_policia + custo_aluguel + atracao 
         return custo_aluguel, custo_bombeiro, custo_policia, custo_festa
     
-    def calcular_impostos(receita_total):
+    def calcular_impostos(self, receita_total):
         iss = receita_total * 0.05  # 5% do total
         icms = receita_total * 0.18  # 18% do total
         cofins = receita_total * 0.03  # 3% do total
         pis = receita_total * 0.0065  # 0.65% do total
-
         return iss, icms, cofins, pis
 
-    def calcular_receita_liquida(self):
-        receita_total = self.calcular_receita_total()
-        iss, icms, cofins, pis = self.calcular_impostos()
+    def calcular_receita_liquida(self, receita_total):
+        iss, icms, cofins, pis = self.calcular_impostos(receita_total)
         receita_liquida = receita_total - iss - icms - cofins - pis
-
         print("A receita líquida após a dedução do ISS, ICMS, COFINS e PIS é: R${:.2f}".format(receita_liquida))
 
-    consumo
-# Crie um novo evento com um custo de R$1000
-meu_evento = Evento(1000)
+# Crie um novo evento
+meu_evento = Evento()
 
-# Adicione algumas receitas
-meu_evento.adicionar_receita("Ingressos", 50, 100) # nome: "Ingressos", preço unitário: R$50, quantidade: 100
-meu_evento.adicionar_receita("Bebidas", 10, 200)  # nome: "Bebidas", preço unitário: R$10, quantidade: 200
-meu_evento.adicionar_receita("Comidas", 20, 150)  # nome: "Comidas", preço unitário: R$20, quantidade: 150
+# Cálculo de custos
+quantidade_pessoas = 2000
+custo_atracao = 10000  # Custo da atração (música, palestra, etc.)
+custo_aluguel, custo_bombeiro, custo_policia, custo_festa = meu_evento.custo(quantidade_pessoas, custo_atracao)
+print("Custo da Atração: R${:.2f}".format(custo_atracao))
+print("Custo do Aluguel: R${:.2f}".format(custo_aluguel))
+print("Custo dos Bombeiros: R${:.2f}".format(custo_bombeiro))
+print("Custo da Polícia: R${:.2f}".format(custo_policia))
+print("Custo Total da Festa: R${:.2f}".format(custo_festa))
 
-# Calcule e imprima a receita total
-print("Receita Total: R${:.2f}".format(meu_evento.calcular_receita_total()))
+# Cálculo de receita
+margem_lucro = 20  # margem de lucro desejada em porcentagem
+receita = meu_evento.receita(custo_festa, margem_lucro)
+print("Receita Necessária (incluindo lucro): R${:.2f}".format(receita))
 
-# Calcule a receita necessária para cobrir o custo do evento
-meu_evento.calcular_receita_necessaria()
+# Cálculo do preço dos ingressos
+preco_ingresso = meu_evento.ingressos(quantidade_pessoas, receita)
+print("Preço do Ingresso: R${:.2f}".format(preco_ingresso))
 
-# Calcule os impostos
-iss, icms = meu_evento.calcular_impostos()
+# Cálculo dos impostos
+iss, icms, cofins, pis = meu_evento.calcular_impostos(receita)
 print("ISS: R${:.2f}".format(iss))
 print("ICMS: R${:.2f}".format(icms))
+print("COFINS: R${:.2f}".format(cofins))
+print("PIS: R${:.2f}".format(pis))
 
-# Calcule e imprima a receita líquida
-meu_evento.calcular_receita_liquida()
-
+# Cálculo da receita líquida
+meu_evento.calcular_receita_liquida(receita)
